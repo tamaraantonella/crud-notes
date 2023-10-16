1e92c3f6d6e16a1451e832d57a9bca8acff12936
tamif <tamarafrazzetta@gmail.com>
2023-10-16 14:47:47 -0300

    feat: add typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
 
+import configModuleBootstrapper from './bootstrappers/config-module.bootstrapper';
+import typeormModuleBootstrapper from './bootstrappers/typeorm-module.bootstrapper';
+
 @Module({
-  imports: [NotesModule],
-  providers: [AppService],
+	imports: [configModuleBootstrapper(),typeormModuleBootstrapper(),NotesModule],
+	providers: [AppService]
 })
 export class AppModule {}
