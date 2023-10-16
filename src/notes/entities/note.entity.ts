411f8d97fe91566fabb1724610bbed71db3e27db
tamif <tamarafrazzetta@gmail.com>
2023-10-16 15:30:45 -0300

    feat: add Note entity

--- src/notes/entities/note.entity.ts
+++ src/notes/entities/note.entity.ts
@@ -1,1 +1,31 @@
-export class Note {}
+import {
+	Column,
+	CreateDateColumn,
+	DeleteDateColumn,
+	Entity,
+	PrimaryGeneratedColumn
+} from 'typeorm';
+
+@Entity()
+export class Note {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column()
+	title: string;
+
+	@Column()
+	content: string;
+
+	@Column()
+	createdBy: number;
+
+	@CreateDateColumn()
+	createdAt: Date;
+
+	@CreateDateColumn()
+	updatedAt: Date;
+
+	@DeleteDateColumn()
+	deletedAt: Date;
+}
