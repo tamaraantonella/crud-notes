23cc9a02e9b3c405ec391fe2b6655886285a31fb
tamif <tamarafrazzetta@gmail.com>
2023-10-16 14:27:43 -0300

    feat: add notes resource

---
+++ src/notes/notes.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import { NotesService } from './notes.service';
+import { CreateNoteDto } from './dto/create-note.dto';
+import { UpdateNoteDto } from './dto/update-note.dto';
+
+@Controller('notes')
+export class NotesController {
+  constructor(private readonly notesService: NotesService) {}
+
+  @Post()
+  create(@Body() createNoteDto: CreateNoteDto) {
+    return this.notesService.create(createNoteDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.notesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    console.log("🚀 ~ file: notes.controller.ts:22 ~ NotesController ~ findOne ~ id:", id)
+    return this.notesService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateNoteDto: UpdateNoteDto) {
+    return this.notesService.update(+id, updateNoteDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.notesService.remove(+id);
+  }
+}
