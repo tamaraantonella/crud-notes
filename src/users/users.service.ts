00a6852cb7750b0e6b955fe414316252b33bcd75
tamif <tamarafrazzetta@gmail.com>
2023-10-18 18:56:16 -0300

    chore: add updateUser to users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
-//import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -23,15 +23,13 @@ export class UsersService {
 		return `This action returns all users`;
 	}
 
-	findOne(id: string) {
-		return `This action returns a #${id} user`;
+	findById(id: string) {
+		return this.userRepository.findOneBy({ id });
 	}
 
-	/*
-  update(id: number, updateUserDto: UpdateUserDto) {
-		return `This action updates a #${id} user`;
+	update(id: string, updateUserDto: UpdateUserDto) {
+		return this.userRepository.update(id, updateUserDto);
 	}
-  */
 
 	remove(id: string) {
 		return `This action removes a #${id} user`;
